99c380f4497b53de01c65cc23aa7b3f370974b56
hudaSharings <hudapossible@gmail.com>
2024-05-29 16:19:44 +0530

    pasword property removed from user responce data

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -12,7 +12,13 @@ app.post("/user", async (c) => {
   const createdUser = await drizzle(c.env.DB)
     .insert(tables.users)
     .values(user)
-    .returning();
+    .returning({
+      id: tables.users.id,
+      userName: tables.users.userName,
+      name: tables.users.name,
+      email: tables.users.email,
+      mobileNumber: tables.users.mobileNumber,
+    });
   return c.json(successResponse(createdUser));
 });
 app.put("/user/:id", async (c) => {
@@ -22,7 +28,13 @@ app.put("/user/:id", async (c) => {
     .update(tables.users)
     .set(user)
     .where(eq(tables.users.id, parseInt(p.id)))
-    .returning();
+    .returning({
+      id: tables.users.id,
+      userName: tables.users.userName,
+      name: tables.users.name,
+      email: tables.users.email,
+      mobileNumber: tables.users.mobileNumber,
+    });
   return c.json(successResponse(updatedUser));
 });
 app.delete("/user/:id", async (c) => {
@@ -30,11 +42,25 @@ app.delete("/user/:id", async (c) => {
   const deletedUser = await drizzle(c.env.DB)
     .delete(tables.users)
     .where(eq(tables.users.id, parseInt(p.id)))
-    .returning();
+    .returning({
+      id: tables.users.id,
+      userName: tables.users.userName,
+      name: tables.users.name,
+      email: tables.users.email,
+      mobileNumber: tables.users.mobileNumber,
+    });
   return c.json(successResponse(deletedUser));
 });
 app.get("/users", async (c) => {
-  const [users] = await drizzle(c.env.DB).select().from(tables.users);
+  const [users] = await drizzle(c.env.DB)
+    .select({
+      id: tables.users.id,
+      userName: tables.users.userName,
+      name: tables.users.name,
+      email: tables.users.email,
+      mobileNumber: tables.users.mobileNumber,
+    })
+    .from(tables.users);
 });
 app.get("/users/:id", async (c) => {
   const p = c.req.param();
